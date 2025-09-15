d66b139625926047ec753e8d793d5fb0f3b5c1c5
Jack Shih <randomdize@gmail.com>
2025-09-15 23:27:58 +0800

    update nixpkgs
    - update hugo version
    - update theme version

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0c6d8c783336a59f4c59d4a6daed6ab269c4b36.tar.gz"){}}:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c6625cd1a34d03926de68d5acc46f160b2a34b7b.tar.gz"){}}:
 pkgs.mkShell {
   packages = with pkgs; [
     git
